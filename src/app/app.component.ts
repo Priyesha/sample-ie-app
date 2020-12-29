79f2b851836a13085f695171066014364a964ed9
Priyesha Yadav <priyesha.yadav@yash.com>
2020-12-29 11:58:46 +0530

    search result added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { Subject } from 'rxjs';
+import { DataService } from './data.service';
+// import { navigationConfig } from './navigate.config';
+import { SearchListConfiguration } from '@gsa-sam/layouts';
+import { FilterService } from './filter.service';
+import { SideNavigationModel } from '@gsa-sam/components';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-demo-app';
+  @ViewChild('resultList') resultList;
+
+  fields: FormlyFieldConfig[] = [];
+  form;
+  filterModel = {};
+  options;
+  filtersExpanded: boolean = true;
+  public filterChange$ = new Subject<object>();
+  // public navigationModel: SideNavigationModel = navigationConfig;
+
+  listConfig: SearchListConfiguration = {
+    defaultSortValue: 'legalBusinessName',
+    pageSize: 25,
+    sortList: [
+      { text: 'Entity Name', value: 'legalBusinessName' },
+      { text: 'Status', value: 'registrationStatus' }
+    ]
+  };
+
+  /* Sort config change demo */
+  defaultListConfig: SearchListConfiguration = {
+    defaultSortValue: 'legalBusinessName',
+    pageSize: 25,
+    sortList: [
+      { text: 'Entity Name', value: 'legalBusinessName' },
+      { text: 'Status', value: 'registrationStatus' }
+    ]
+  };
+
+  updatedListConfig: SearchListConfiguration = {
+    defaultSortValue: 'cageCode',
+    pageSize: 25,
+    sortList: [
+      { text: 'Entity Name', value: 'legalBusinessName' },
+      { text: 'Cage Code', value: 'cageCode' },
+      { text: 'Status', value: 'registrationStatus' }
+    ]
+  };
+
+  constructor(
+    public service: DataService,
+    public filterService: FilterService
+  ) {}
+  ngOnInit() {
+    this.fields = this.filterService.fields;
+    this.filterModel = this.filterService.model;
+    this.form = this.filterService.form;
+    this.options = this.filterService.options;
+  }
+  ngAfterViewInit() {
+    this.filterChange$.subscribe(res => {
+      this.resultList.updateFilter(res);
+    });
+  }
+
+  updateConfig(update: boolean) {
+    if(update) {
+      this.listConfig = { ...this.updatedListConfig };
+    }
+    else {
+      this.listConfig = { ...this.defaultListConfig };
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from "@angular/common";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  SdsSearchResultListModule,
+  SdsToolbarModule,
+  SdsSideNavigationModule,
+  SdsAccordionModule,
+  SdsSelectionPanelModule
+} from '@gsa-sam/components';
+import {
+  SdsFiltersModule
+} from '@gsa-sam/sam-formly';
+
+import { SearchListServiceModule, SideToolbarModule } from '@gsa-sam/layouts';
+import { FilterService } from './filter.service';
+import { sds } from '@gsa-sam/sam-styles/src/icons/';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import {
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// const routes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -10,9 +34,28 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule,
+    CommonModule,
+    FontAwesomeModule,
+    SdsToolbarModule,
+    SdsAccordionModule,
+    SdsSideNavigationModule,
+    SdsFiltersModule,
+    SdsSearchResultListModule,
+    SearchListServiceModule,
+    SideToolbarModule,
+    SdsSelectionPanelModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    RouterModule.forRoot([]),
+    FormsModule,
   ],
-  providers: [],
+  providers: [FilterService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, sds);
+  }
+}
